83e3d7a5587a47f5bf46b2b0b054c5d0206529a8
moudud <sihamislam18@gmail.com>
2025-02-22 21:03:54 +0600

    loading a single page

--- src/Components/Gamingcard/Gamingcard.jsx
+++ src/Components/Gamingcard/Gamingcard.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import './Gameingcard.css';
 
-const Gamingcard = ({game}) => {
-    console.log(game)
+const GameCard = ({ game }) => {
     return (
-        <div>
-            <h1>gamig card</h1>
+        <div className="game-card">
+            <div className="game-image">
+                <img src={game.image} alt={game.title} />
+            </div>
+            <div className="game-details">
+                <div className="game-category">
+                    {game.category}
+                    <span className="arrow-icon">▶</span>
+                </div>
+                <h2 className="game-title">{game.title}</h2>
+                <div className="game-platform">
+                    <span className="platform-icon"></span> {game.platform}
+                </div>
+                <div className="game-rating">
+                    {game.rating}
+                </div>
+                <div className="game-year">
+                    {game.year}
+                </div>
+                <p className="game-description">{game.description}</p>
+                <button className="explore-details-button">Explore Details</button>
+            </div>
         </div>
     );
 };
 
-export default Gamingcard;
+export default GameCard;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import Banner from '../Banner/Banner';
 import Minicard from '../Minicard/Minicard';
 import { useLoaderData } from 'react-router-dom';
 import Gamingcard from '../Gamingcard/Gamingcard';
-
+import "./Home.css"
 
 const Home = () => {
     const gameloader=useLoaderData();
@@ -12,7 +12,7 @@ const Home = () => {
         <div>
          <Banner></Banner>
           <Minicard></Minicard>
-          <div>
+          <div className='grid grid-cols-3 gap-4 justify-center mx-auto game-card-container'>
             {
                 gameloader.map(game=><Gamingcard key={game.id} game={game}></Gamingcard>)
             }
